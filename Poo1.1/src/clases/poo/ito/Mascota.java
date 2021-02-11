2f21934650798d7b9d596516009a01a1ae2e45cf
rherr <rherr@DESKTOP-A2FLHKB>
2021-02-10 22:08:47 -0600

    Agregar propiedad a Mascota

--- Poo1.1/src/clases/poo/ito/Mascota.java
+++ Poo1.1/src/clases/poo/ito/Mascota.java
@@ -1,11 +1,10 @@
 package clases.poo.ito;
 
 public class Mascota {
-   private String clasificacion;	
-   private String raza;
-   private String genero;
-   private String nombre;
-   private int edad;
-   
-   
+	private String clasificacion;
+	private String raza;
+	private String genero;
+	private String nombre;
+	private int edad;
+
 }
--- Poo1.1/src/poo/ito/Principal.java
+++ Poo1.1/src/poo/ito/Principal.java
@@ -5,14 +5,14 @@ import clases.poo.ito.Mascota;
 public class Principal {
 
 	static Mascota m;
-	
+
 	static void run() {
-	    	
+
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       run();
+		run();
 	}
 
 }
