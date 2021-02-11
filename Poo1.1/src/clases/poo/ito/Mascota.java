429f5d0a0038380fd03b90c6022873314493fe2b
rherr <rherr@DESKTOP-A2FLHKB>
2021-02-10 22:01:00 -0600

    Agregar Clase generadas

---
+++ Poo1.1/src/clases/poo/ito/Mascota.java
@@ -0,0 +1,11 @@
+package clases.poo.ito;
+
+public class Mascota {
+   private String clasificacion;	
+   private String raza;
+   private String genero;
+   private String nombre;
+   private int edad;
+   
+   
+}
---
+++ Poo1.1/src/poo/ito/Principal.java
@@ -0,0 +1,18 @@
+package poo.ito;
+
+import clases.poo.ito.Mascota;
+
+public class Principal {
+
+	static Mascota m;
+	
+	static void run() {
+	    	
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       run();
+	}
+
+}
